7d8c6e7fc85890d16f8c158b8c03e1aef7154327
Jack Lawson <ajacksified@gmail.com>
2020-11-30 20:10:03 -0500

    Report 21

--- components/intro.js
+++ components/intro.js
@@ -4,24 +4,26 @@ import Link from './link';
 import styles from './styles';
 import config from '../config';
 
-export default function Intro({ children }) {
+export default function Intro({ children, showUniform = true }) {
   return (
     <Card>
       <h5 style={{ ...styles.h5, marginBottom: '1em' }}>
         {config.cmdr.intro}
       </h5>
 
-      <Link
-        href={`https://tc.emperorshammer.org/record.php?pin=${config.cmdr.pin}&type=profile`}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          style={{ width: '100%', maxWidth: '190px', float: 'right' }}
-          src="https://tempest-blown-with-the-wind.vercel.app/uniform.jpg"
-          alt={`The uniform of ${config.cmdr.name}, #${config.cmdr.pin}`}
-        />
-      </Link>
+      { showUniform && (
+        <Link
+          href={`https://tc.emperorshammer.org/record.php?pin=${config.cmdr.pin}&type=profile`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            style={{ width: '100%', maxWidth: '190px', float: 'right' }}
+            src="https://tempest-blown-with-the-wind.vercel.app/uniform.jpg"
+            alt={`The uniform of ${config.cmdr.name}, #${config.cmdr.pin}`}
+          />
+        </Link>
+      )}
 
       {children}
     </Card>
