aebe13f44781c3cc0ae8d7a60d81ca427d32115e
EH-Wreckage <86890656+EH-Wreckage@users.noreply.github.com>
2022-08-28 12:29:22 -0700

    Update 2.js

--- pages/reports/2.js
+++ pages/reports/2.js
@@ -94,7 +94,7 @@ const ACTIVITY = {
     communication: 'Discord',
     flightActivity: '',
     otherActivity: '',
-    notes: 'Hope you had a great vacation. Time to get back t blowing up Rebels!',
+    notes: 'Hope you had a great vacation. Time to get back to blowing up Rebels!',
   },
   // Flax Rogers
   56344: {
@@ -132,7 +132,7 @@ const ACTIVITY = {
 
   // Billy Lee
   56358: {
-    communication: 'Discord',
+    communication: 'None',
     flightActivity: '',
     otherActivity: '',
     notes: 'Have not heard from you. please reach out.',
@@ -142,9 +142,9 @@ const ACTIVITY = {
     // Flik Metis
     56378: {
       communication: 'Discord',
-      flightActivity: '',
+      flightActivity: 'SWS',
       otherActivity: '',
-      notes: 'Welcome to Sigma!',
+      notes: 'Great job getting promoted',
     },
 
 
@@ -153,7 +153,7 @@ const ACTIVITY = {
   55758: {
     communication: 'Discord',
     flightActivity: 'TC Battles, PvE Flying',
-    otherActivity: 'Signal Scramble Infection',
+    otherActivity: 'Signal Scramble Infection, Runng 2 competitions.',
     notes: 'Thank you for running those comps!',
   },
 
@@ -197,17 +197,12 @@ export default function Report({ activityData }) {
 
         <center><h4><span style={{ color: "#401562" }}>Squadron Commander's Speech</span></h4></center>
         <p>
-        Welcome to the inaugural Squadron Commander Report, Sigma Squad. I'm happy to be here and contribute to restoring Sigma to its former greatness. 
-        We have a lot of work ahead of us, but fortunately, we have two other excellent pilots who also make amazing leaders. I'm thrilled to have CM JetMech 
-        as my SQXO and LC randyrumrnr as a Flight Leader. I have no doubt that we can launch Sigma and take off quickly with their assistance. Everyone has a 
-        lot of Rebel ships to destroy.
+        Hey Sigma, hope all is well. I know this month has been kind of a crazy month with the whole squad getting situated. We are off 
+        and running and even had a SL get promoted recently. Congrats to our very own now LT Flik Metis. Get Flik a drink the next to you fly with him.
         </p>
-		    <p>There are many fresh faces here. Many of you are not only new to Sigma, but also to The Emperor's Hammer. No worries, it may seem like a lot at first, 
-          but in the end, this is a place where flying, gaming, and having fun are the only things that matter. Do not hesitate to contact me, CM JetMech, or LC 
-          Randyrumrnr if you have any questions at all. We are all eager to assist you and make your time at the Emperor's Hammer as enjoyable as we can.</p>
-        <p>We have many competitions and ideas brewing here in Sigma. Once all the dust settles and everyone is ready for launch we will be getting those under way. So keep an eye out.  </p>
-        <p>Lastly, I have finished the MSE for the month of July. Many of you were either a part of another squadron or were too new to engage in any activity. Dont worry, we still have plenty
-           of time to earn a medal for the month of August. Reach out to me if you have any questions about what an MSE is, and I'll be happy to clarify.  Until then, get out and kill some Rebels!</p>
+		    <p>We also have some Sigma Comps going on. Check out the competitions section down below and get a submission in.</p>
+        <p>August is almost over so get those last minute MSE points in so you can get yourself a nice shiny medal. </p>
+        <p>Prepare yourselves,  RTF is coming.</p>
 
 
       </Intro>
@@ -216,16 +211,14 @@ export default function Report({ activityData }) {
 
                 <center><h3><span style={{ color: "#401562" }}>Squadron News</span></h3></center>
         <ul>
-                <li>Sigma Squadron has been activated!</li><br></br>
-                <li>Congrats to CM JetMech on his SQXO promotion.  </li><br></br>
-                <li>Also congrats to LC randyrumrnr for becoming FL of FLight III</li><br></br>
-                <li>Sigma Squadron competitions are coming, until then go complete some Warrior and TC comps.</li><br></br>
+                <li>Conrats to LT Flik Metis on promotion</li><br></br>
+                <li>Get you comp submissions in  </li><br></br>
         </ul>
                 <center><h3><span style={{ color: "#401562" }}>Fleet News</span></h3></center>
         <ul>
-            	<li>SQXO will now be a primary position taking the place of the Flight Leader 2-1 position in every squadron.</li><br></br>
-                <li>CM Sylas "Professor" Pitt has been appointed the new Training Officer  </li><br></br>
-                <li>LC Robert Hogan has been selected to be a TOA   </li><br></br>
+            	<li>TCiB is live, fly you fools</li><br></br>
+                <li>Congrats to Warriors own Westric Davalorn and SkyShadow on their well deserved promotions  </li><br></br>
+                <li>RTF will be here very soon  </li><br></br>
         </ul>
 
       </Orders>
@@ -240,8 +233,9 @@ export default function Report({ activityData }) {
       />
 
       <Closing>
-      I will conclude by saying that I am happy to be back. I believe this squad is coming together nicely. We are getting ready to accomplish something wonderful. 
-      RTF will arrive soon, and when it does, Sigma will be prepared. Don't forget you can always shoot me DM in Discord or an email if you need anything.   
+      In closing, there are a few of you we have been trying to reach. We understand that real 
+      life gets busy, especially right now with school starting back up and everyone being tied up in work. All we 
+      ask is that you communicate with us.  Remember to reach out to myself or your flight leader if you have any questions or concerns.  
   
       <p>MAJ Wreckage</p>
       <i><span style={{ color: "#bb0a1e" }}>"Let the sky fall"</span></i>
